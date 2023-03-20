9c6c3ee09d111f99cb7b6c1c3b95713067d6ce8b
smkvs <smkvs@192.168.1.74>
2023-03-20 10:56:05 +0530

    Old programs added

---
+++ src/main/java/Javatraining/Program1.java
@@ -0,0 +1,20 @@
+package Javatraining;
+/**
+ * 
+ */
+
+/**
+ * @author smkvs
+ *
+ */
+//public class Program1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hello world");
+	}
+
+}
---
+++ src/main/java/Javatraining/program.java
@@ -0,0 +1,28 @@
+package Javatraining;
+
+import java.util.Scanner;
+
+public class program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     Scanner sc=new Scanner(System.in);
+     System.out.println("enter the value of n:");
+     int n= sc.nextInt();
+     for (int i=1;i<=n;i++) {
+    	 for(int j=1;j<=n;j++) {
+    		 if(i==1|| i==n|| j==1|| j==n) {
+    			 System.out.print("$");
+    		 }
+    		 else {
+    			 System.out.print("-");
+    		 }
+    		 
+    	 }
+    	 System.out.println();
+    	 
+     }
+ 
+	}
+
+}
