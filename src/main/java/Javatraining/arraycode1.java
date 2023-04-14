b23371bbdb13c75419df8d44042530fcfb5a57fc
smkvs <smkvs@LAPTOP-2MOK6RJ5>
2023-04-14 13:34:01 +0530

    array code 1

---
+++ src/main/java/Javatraining/arraycode1.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package Javatraining;
+
+/**
+ * @author smkvs
+ *
+ */
+import java.util.Arrays;
+public class arraycode1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[]= {10,20,21,22,23,24,25,26,27,29};
+		Arrays.sort(arr);
+		for(int i=0;i<arr.length;i++) {
+			System.out.print(arr[i]+" ");
+		}
+
+	}
+
+}
